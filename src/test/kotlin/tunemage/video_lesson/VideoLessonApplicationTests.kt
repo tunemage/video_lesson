b5dd9a85b7f417f9a63798a4f5f2e48555d72078
DESKTOP-2D1M2F4\tomita.tsunetaka <tune1983@gmail.com>
2022-03-30 23:21:34 +0900

    spring initialazerで初期コードを生成

---
+++ src/test/kotlin/tunemage/video_lesson/VideoLessonApplicationTests.kt
@@ -0,0 +1,13 @@
+package tunemage.video_lesson
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VideoLessonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
