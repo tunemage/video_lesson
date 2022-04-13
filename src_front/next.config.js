1a75d08ae73e141136cf28bb0ccdf6350e9b5476
DESKTOP-2D1M2F4\tomita.tsunetaka <tune1983@gmail.com>
2022-04-13 23:12:03 +0900

    Win11,WSL2,dockerの環境でnext.jsのホットリロードが効かない問題に対応

--- src_front/next.config.js
+++ src_front/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Win11,WSL2,dockerの環境でnext.jsのホットリロードが効かない問題に対応
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 800,
+      aggregateTimeout: 300,
+  }
+  return config
+  },
 }
 
 module.exports = nextConfig
